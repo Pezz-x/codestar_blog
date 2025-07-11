409e271e2f6ef1c476d6d18495518faef0b7cff1
Pezz-x <145380526+Pezz-x@users.noreply.github.com>
2025-07-11 13:24:35 +0100

    match to source code

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^10m7#3bvx2+dukdf=&n_4-f43=gow2bc8l6c2egf!o+b+5&35
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com',]
+ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1']
 
 
 # Application definition
