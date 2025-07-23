7843898b53fd9aeba861d21abfd11f43dddddf56
Pezz-x <145380526+Pezz-x@users.noreply.github.com>
2025-07-23 14:19:45 +0100

    finished walkthrough

--- blog/views.py
+++ blog/views.py
@@ -31,7 +31,6 @@ def post_detail(request, slug):
     comment_count = post.comments.filter(approved=True).count()
 
     if request.method == "POST":
-        print("Recieved a POST request")
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
             comment = comment_form.save(commit=False)
@@ -44,7 +43,6 @@ def post_detail(request, slug):
         )
 
     comment_form = CommentForm()
-    print("About to render template")
     return render(
         request,
         "blog/post_detail.html",
